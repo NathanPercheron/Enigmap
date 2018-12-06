9ec9bbe87abafa9a654d0130d36cc9ccfb22e284
Nathan Percheron <percheron.nathan@outlook.fr>
2018-12-06 10:50:00 +0100

    Les vues sont crées

---
+++ Enigmap/Enigmap/Vues/AccueilAuthentification.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Enigmap.Vues
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AccueilAuthentification : ContentPage
+	{
+		public AccueilAuthentification ()
+		{
+			InitializeComponent ();
+		}
+
+        private void BoutonConnexion_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BoutonInscription_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
