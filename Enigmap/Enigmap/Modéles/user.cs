db96d7ac6fe62f20054ebb99da1b9b0036d1a8d6
Nathan Percheron <percheron.nathan@outlook.fr>
2018-12-10 11:05:24 +0100

    Création de la classe "user" et ajouts de ses atributs Création de la classe "ApiUser"

--- Enigmap/Enigmap/Modéles/user.cs
+++ Enigmap/Enigmap/Modéles/user.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,20 @@ namespace Enigmap.Modéles
 {
     class user
     {
+        private string _username;
+        private string _password;
+
+        public static ArrayList CollClassUser = new ArrayList();
+
+
+
+        public user(string username, string password)
+        {
+            _username = username;
+            _password = password;
+        }
+
+        public string Username { get => _username; set => _username = value; }
+        public string Password { get => _password; set => _password = value; }
     }
 }
